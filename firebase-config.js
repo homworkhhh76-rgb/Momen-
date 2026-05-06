77213b5a40ebd16ee081b39c35a0cf25b9a80e8d
homworkhhh76-rgb <homworkhhh76@gmail.com>
2026-05-06 14:36:33 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBmFvdRJX64UEWw11eu9buhPc6kVvxVx_U",
+    authDomain: "momen-1d40d.firebaseapp.com",
+    databaseURL: "https://momen-1d40d-default-rtdb.firebaseio.com",
+    projectId: "momen-1d40d",
+    storageBucket: "momen-1d40d.firebasestorage.app",
+    messagingSenderId: "978169521529",
+    appId: "1:978169521529:web:37d8260872e0ee3561fba8",
+    measurementId: "G-YGWN79ZB3J"
+  },
+
+  firebaseRoot: "cashier_units_pro_momen_1d40d_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_momen_1d40d_885766842"
+  ]
+};
